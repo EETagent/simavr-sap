ed99c3037d43c20ca2a034f364b4a45d8bd1576f
Vojtěch Jungmann <vojta.jungmann@gmail.com>
2024-04-02 22:45:56 +0200

    feat: display.asm as example

--- sap/program.asm
+++ sap/program.asm
@@ -1,12 +1,22 @@
 .include "asm/m328Pdef.inc"
 
+; podprogramy pro praci s displejem
+.org 0x1000 ; <1>
+.include "asm/printlib.inc"
+
 ; Zacatek programu - po resetu
 .org 0
-jmp start
+    jmp start
 
 ; Zacatek programu - hlavni program
+.org 0x100
 start:
-    ldi r16, 42
-    ldi r17, 100
+    ; Inicializace displeje
+    call init_disp
     
-end: rjmp end ; Zastavime program - nekonecna smycka 3
+    ; *** ZDE muzeme psat nase instrukce
+    ldi r16, '0'    ; znak
+    ldi r17, 0      ; pozice (0x00-0x0f - prvni radek; 0x40-0x4f - druhy radek)
+    call show_char  ; zobraz znak z R16 na pozici z R17
+
+end: jmp end        ; Zastavime program - nekonecna smycka
