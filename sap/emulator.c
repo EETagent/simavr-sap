6e966da85f73b914a4f20ee3048e93e9846b25f3
Vojtěch Jungmann <vojta.jungmann@gmail.com>
2024-04-04 15:29:02 +0200

    repo: cleanup & add modified disclaimer

--- sap/emulator.c
+++ sap/emulator.c
@@ -18,6 +18,9 @@
 
         You should have received a copy of the GNU General Public License
         along with simavr.  If not, see <http://www.gnu.org/licenses/>.
+
+
+        Modified by EETagent
  */
 
 #include <libgen.h>
